858cb64f81e36064efac028cb425bb30cf52c5a4
bangaradi <adityavb@cse.iitk.ac.in>
2023-05-17 17:41:33 +0530

    ass1 done

--- uwimg.py
+++ uwimg.py
@@ -17,7 +17,6 @@ class IMAGE(Structure):
 
 #lib = CDLL("/home/pjreddie/documents/455/libuwimg.so", RTLD_GLOBAL)
 #lib = CDLL("libuwimg.so", RTLD_GLOBAL)
-print("printing this shitttttttttt =t==== tt= t",os.path.dirname(__file__))
 lib = CDLL(os.path.join(os.path.dirname(__file__), "libuwimg.so"), RTLD_GLOBAL)
 
 make_image = lib.make_image
